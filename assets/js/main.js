6a18c76b0cb67a9c5f2bc163bcd1aa33cfa34789
kuro-96 <ng.ngocanhtuan96@gmail.com>
2020-02-18 09:29:03 +0700

    add header + footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
-$('.dropdown').on('click', function(e) {
-    var target = $(e.target);
-    var dropdown = target.closest('.dropdown');
-    return !dropdown.hasClass('open') || !target.hasClass('dropdown-menu__search__input');
-});
+$(document).ready(function () {
+  $('.product__title').click(function () {
+    $('.product__list').toggleClass('active');
+  })
+  $(".product-nav__dropdown-btn").click(function () {
+    $(".product-nav__dropdown-menu").toggleClass('active');
+  });
+  $(".wrap__menu-btn").click(function() {
+		$(this).children('i').toggleClass("active");
+		$(".wrap__menu-dropdown").toggleClass("active");
+  });
+  
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > $('.desktop__nav').offset().top)
+  //     $(".desktop__nav").addClass('active');
+  //   else if ($(this).scrollTop() <= $('.desktop__nav').offset().top)
+  //     $(".desktop__nav").removeClass("active");
+  // })
+})
